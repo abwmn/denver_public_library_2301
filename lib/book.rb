3fc3599b3935592eab93e4f7afb4303f233dfa4b
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-24 11:26:09 -0500

    feat: book class with details

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,10 @@
 class Book
-  
+  attr_reader :title, :author, :publication_year
+
+  def initialize(details)
+    @title = details[:title]
+    @author = "#{details[:author_first_name]} #{details[:author_last_name]}"
+    # @publication_date = details[:publication_date]
+    @publication_year = details[:publication_date].slice(-4,4)
+  end
 end
