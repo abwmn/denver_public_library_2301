e5c5a607c8f38b3c2630b2c5c67818687a74752e
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-24 12:04:47 -0500

    test: library

---
+++ spec/library_spec.rb
@@ -0,0 +1,32 @@
+require './lib/book'
+require './lib/author'
+require './lib/library'
+
+RSpec.describe Library do
+  before(:each) do
+    @library = Library.new("Aurora Springs")
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    @jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @villette = @charlotte_bronte.write("Villette", "1853")
+  end
+  it 'exists' do
+    expect(@library).to be_a(Library)
+  end
+
+  it 'has a name' do
+    expect(@library.name).to eq("Aurora Springs")
+  end
+
+  it 'can add authors' do
+    expect(@library.authors).to eq([])
+    @library.add_author(@charlotte_bronte)
+    expect(@library.authors).to eq([@charlotte_bronte])
+  end
+
+  it 'can add books' do
+    expect(@library.authors).to eq([])
+    expect(@library.books).to eq([])
+    @library.add_author(@charlotte_bronte)
+    expect(@library.books).to eq([@jane_eyre, @villette])
+  end
+end
