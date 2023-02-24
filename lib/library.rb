3926a998d9c549adef4d450103319300e2028c75
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-24 12:51:36 -0500

    feat: check out/return books

--- lib/library.rb
+++ lib/library.rb
@@ -22,4 +22,25 @@ class Library
     end
     time_frame = {start: book_dates.min.to_s, end: book_dates.max.to_s} 
   end
+
+  def check_out(book)
+    book.checked_out = true
+    book.times_checked_out += 1
+  end
+
+  def return(book)
+    book.checked_out = false
+  end
+
+  def checked_out
+    @books.find_all{|book| book.checked_out}
+  end
+
+  def most_checked_out
+    most_checkouts = 0
+    @books.each do |book|
+      most_checkouts = [most_checkouts, book.times_checked_out].max
+    end
+    @books.find{|book| book.times_checked_out == most_checkouts}
+  end
 end
