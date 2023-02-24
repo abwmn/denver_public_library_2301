972d6fe49dc748c6455183a5e1dfe791adfa8004
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-24 12:03:55 -0500

    feat: author class

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,12 @@
 class Author
+  attr_reader :name, :books
+  def initialize(name)
+    @name = "#{name[:first_name]} #{name[:last_name]}"
+    @books = []
+  end
 
+  def write(title, date)
+    @books << book = Book.new({title: title, publication_date: date, author_name: @name})
+    book
+  end
 end
