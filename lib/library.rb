8e3b03025eab46067d9482a09cedc94f25085f70
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-24 12:28:15 -0500

    feat: library can show publication timeframe

--- lib/library.rb
+++ lib/library.rb
@@ -8,9 +8,18 @@ class Library
   end
 
   def add_author(author)
-    @authors << author
     author.books.each do |book|
       @books << book
     end
+    @authors << author
+  end
+
+  def publication_time_frame_for(author)
+    books = @books.find_all{|book| book.author == author}
+    book_dates = []
+    books.each do |book|
+      book_dates << book.publication_year.to_i
+    end
+    time_frame = {start: book_dates.min.to_s, end: book_dates.max.to_s} 
   end
 end
