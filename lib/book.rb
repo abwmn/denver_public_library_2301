1966f04b05a26a357e55b0cbc2ddd694eb8d1fff
Andrew Bowman <andrewtbowman@gmail.com>
2023-02-24 12:52:19 -0500

    feat: track checked_out and times_checked_out

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,16 @@
 class Book
-  attr_reader :title, :author, :publication_year
+  attr_reader :title, :author, :publication_date, :publication_year
+  attr_accessor :checked_out, :times_checked_out
 
   def initialize(details)
     @title = details[:title]
-    @author = "#{details[:author_first_name]} #{details[:author_last_name]}"
-    # @publication_date = details[:publication_date]
+    if details[:author_first_name]
+      @author = "#{details[:author_first_name]} #{details[:author_last_name]}"
+    else
+      @author = details[:author_name]
+    end
     @publication_year = details[:publication_date].slice(-4,4)
+    @checked_out = false
+    @times_checked_out = 0
   end
 end
